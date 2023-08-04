2af5eae1fcbd48d15a5f4c0ca8277d316b40c0f9
Thiago Alves <tma.bigdata@gmail.com>
2023-08-04 17:36:28 -0300

    Update Cod

--- Python_S3_Redshift/scripts/upload_to_s3.py
+++ Python_S3_Redshift/scripts/upload_to_s3.py
@@ -1,11 +1,12 @@
 import boto3
 
-s3 = boto3.client('s3')
-bucket_name = 'seu-bucket-aqui'  # Substitua pelo nome real do seu bucket
+s3 = boto3.resource(
+    service_name='s3',
+    region_name = 'sa-east-1',
+    aws_access_key_id = '<-- Key ID -->',
+    aws_secret_access_key = '<-- Secret Key -->'
+    )
 
-file_name = 'data/sales.csv'  # Caminho para o arquivo de dados
-
-# Faz o upload do arquivo para o S3
-s3.upload_file(file_name, bucket_name, 'sales/sales.csv')
+s3.meta.client.upload_file('/content/sales.csv', 'seu-bucket-aqui', 'sales/sales.csv')
 
 print('Arquivo enviado para o Amazon S3 com sucesso!')
