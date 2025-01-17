54047aaa9219c8258fef876605daee27c02da4a5
tma.bigdata@gmail.com <tma.bigdata@gmail.com>
2025-01-17 02:43:48 +0000

    refactored code

--- Airflow_Azure_Databricks_Pipeline/notebooks/1_Data_Extract.py
+++ Airflow_Azure_Databricks_Pipeline/notebooks/1_Data_Extract.py
@@ -1,7 +1,10 @@
 # Databricks notebook source
-def get_exchange_rate(date, base='BRL'):
+import requests
+from pyspark.sql.functions import lit
 
-    import requests
+# COMMAND ----------
+
+def get_exchange_rate(date, base='BRL'):
 
     url = f"https://api.apilayer.com/exchangerates_data/{date}&base={base}"
 
@@ -20,8 +23,23 @@ def get_exchange_rate(date, base='BRL'):
 
 # COMMAND ----------
 
-get_exchange_rate('2024-06-16', 'BRL')
+def extract_exchange_rate(data_json):
+    tuple_data = [(currency, float(rate)) for currency, rate in data_json['rates'].items()]
+    return tuple_data
 
 # COMMAND ----------
 
+def save_file_parquet(get_exchange_rate):
+    year, mounth, day = get_exchange_rate['date'].split('-')
+    path = f'dbfs:/databricks-results/bronze/exchange_rate/{year}/{mounth}/{day}'
+    response = extract_exchange_rate(get_exchange_rate)
+    df_conversions = spark.createDataFrame(response, schema='currency string, rate double')
+    df_conversions = df_conversions.withColumn('date', lit(f'{year}-{mounth}-{day}'))
+    df_conversions.write.format('parquet').mode('overwrite').save(path)
 
+    print(f'Data saved in {path}')
+
+# COMMAND ----------
+
+cotations = get_exchange_rate('2024-10-22')
+save_file_parquet(cotations)
