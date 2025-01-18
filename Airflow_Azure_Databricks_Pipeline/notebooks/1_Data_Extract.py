b9b127a04f50f6121c24439d57cc5ad3fce327b8
tma.bigdata@gmail.com <tma.bigdata@gmail.com>
2025-01-18 22:26:29 +0000

    2 mounths of data etl

--- Airflow_Azure_Databricks_Pipeline/notebooks/1_Data_Extract.py
+++ Airflow_Azure_Databricks_Pipeline/notebooks/1_Data_Extract.py
@@ -12,11 +12,11 @@ try:
     execion_date = "{{ ds }}"
 except NameError:
     # If the code is not in Airflow, set a manual test date
-    execion_date = '2024-12-01'  # Change this date to any necessary test value within the last 2 months
+    execion_date = '2024-11-25'  # Change this date to any necessary test value within the last 2 months
 
 # If the execution is not in Airflow, the variable {{ ds }} will not be replaced
 if execion_date == "{{ ds }}":
-    execion_date = '2024-12-01'  # Ensure a default test date is set within the last 2 months
+    execion_date = '2024-11-25'  # Ensure a default test date is set within the last 2 months
 
 # Validate the execution_date parameter
 try:
@@ -61,19 +61,19 @@ def extract_exchange_rate(data_json):
     return [(currency, float(rate)) for currency, rate in data_json['rates'].items()]
 
 # Function to save data in Parquet format
-def save_file_parquet(exchange_data):
+def save_file_parquet(exchange_data, date_str):
     """
     Function that saves the extracted exchange rates in Parquet format in Databricks.
     """
     # Extract the date in the appropriate format
-    year, month, day = exchange_data['date'].split('-')
+    year, month, day = date_str.split('-')
     path = f'dbfs:/databricks-results/bronze/exchange_rate/{year}/{month}/{day}'
     print(f"Saving data to path: {path}...")
 
     # Extract exchange rates and create a DataFrame
     response = extract_exchange_rate(exchange_data)
     df_conversions = spark.createDataFrame(response, schema='currency string, rate double')
-    df_conversions = df_conversions.withColumn('date', lit(f'{year}-{month}-{day}'))
+    df_conversions = df_conversions.withColumn('date', lit(date_str))
     
     # Save the data in Parquet format
     df_conversions.write.format('parquet').mode('overwrite').save(path)
@@ -82,10 +82,23 @@ def save_file_parquet(exchange_data):
 # Main processing
 print("Starting the process...")
 
-# Get exchange rates for the execution date
-cotations = get_exchange_rate(execion_date)
+# Calculate the date range from execion_date to the day before today (yesterday)
+end_date = datetime.today() - timedelta(days=1)  # Yesterday's date
+current_date = exec_date_obj
 
-# Save the data in Parquet
-save_file_parquet(cotations)
+# Loop through the date range
+while current_date <= end_date:
+    date_str = current_date.strftime('%Y-%m-%d')
+    
+    # Get exchange rates for the current date
+    print(f"Processing data for {date_str}...")
+    cotations = get_exchange_rate(date_str)
+
+    # Save the data in Parquet
+    save_file_parquet(cotations, date_str)
+    
+    # Increment the current date by one day
+    current_date += timedelta(days=1)
 
 print("Process completed.")
+
